c93b8ef89424637559b9a8a0e23e9929dd9d11c8
Genzzi-In <admin@genzzi.in>
2026-08-26 18:42:12 +0530

    abishek-react

---
+++ abishek/react/basic_1/src/App.jsx
@@ -0,0 +1,21 @@
+import Heading from "./components/Heading"
+export default function App(){
+  return (
+    <>
+    <Heading />
+    </>
+  )
+}
+
+
+// const Add = ()=>{
+//   return (
+//     <>
+//     <h1>Hii</h1>
+//     <h1>Hii</h1>
+//     </>
+//   )
+// }
+
+// export default App;
+
---
+++ abishek/react/basic_1/src/components/Heading.jsx
@@ -0,0 +1,7 @@
+export default function Heading(){
+    return (
+        <>
+        <h1 className="heading">Welcome to react</h1>
+        </>
+    )
+}
